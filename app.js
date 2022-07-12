be1a5712e0ec242b1b2a15e0ed16991e214c3f50
stgodjah <god@fastfen.club>
2022-07-12 06:12:55 +0000

    get started

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const message = 'Hello World';
-console.log(message, 'from Gitpod!');
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
