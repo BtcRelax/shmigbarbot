471f2bb545596f2a5c85f3927a33e1c996e3ae29
godJah <43356590+stgodjah@users.noreply.github.com>
2023-03-01 15:49:17 +0000

    add command get_balance

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const db = require('./db');
 // your token from BotFather
-const token = process.env.BOT_TOKEN; 
+const token = process.env.BOT_TOKEN;
+
 const Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
     bot.onText(/^\/echo(.+)$/, function (msg, match) {
@@ -38,6 +39,27 @@ const Bot = require('node-telegram-bot-api'),
         });
     });
          
+    bot.onText(/^\/get_balance/,function (msg, match) {
+        /// Get balance anjd return result 
+        var axios = require('axios');
+        const kuna_api_url  = process.env.KUNA_API_URL;
+        var config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: 'http://' + kuna_api_url + '/api/kuna/getbalance',
+                headers: { }
+                };
+
+                axios(config)
+                .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                })
+                .catch(function (error) {
+                console.log(error);
+                });
+
+    } );
+
 
     bot.onText(/^\/get_logs$/, (msg, match) => {
         db.getLogs(res).then(() => {
