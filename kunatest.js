ce079b3a0965a7b2bfd2eb10ceefe8cb58369752
godJah <43356590+stgodjah@users.noreply.github.com>
2023-03-02 14:13:31 +0000

    added command to activate kunacode

--- kunatest.js
+++ kunatest.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 var axios = require('axios');
+var prompt = require('prompt-sync')();
+
 var kuna_api_url  = process.env.KUNA_API_URL;
+var axios = require('axios');
+
+var kunacode = prompt('Get me kuna code:');
+
+var data = JSON.stringify({
+  "code": kunacode
+});
 
 var config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: `http://${kuna_api_url}/api/kuna/getbalance`,
-        headers: { }
-        };
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: `http://${kuna_api_url}/api/kuna/kunacodeactivate`,
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
 
-        axios(config)
-        .then(function (response) {
-        console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-        console.log(error);
-        });
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
